bb5247c3b60127b5c405be7e3d14a47d500dc5f3
Tore Bergebakken <toberge@stud.ntnu.no>
2021-06-01 11:45:55 +0200

    Restructure command code

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,16 @@
 
 module Main where
 
-import Command
-import Control.Monad (when)
-import Data.Text
-  ( Text,
-    isPrefixOf,
-    toLower,
-  )
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-import Discord
-import qualified Discord.Requests as R
-import Discord.Types
-import UnliftIO.Concurrent
+import           Command
+import           Control.Monad                  ( unless
+                                                , when
+                                                )
+import qualified Data.Text                     as T
+import qualified Data.Text.IO                  as TIO
+import           Discord
+import qualified Discord.Requests              as R
+import           Discord.Types
+import           UnliftIO.Concurrent
 
 -- For the time being, a lot of things are very similar to the discord-haskell example in its README
 -- and the ping-pong example.
@@ -23,43 +20,36 @@ import UnliftIO.Concurrent
 main :: IO ()
 main = do
   TIO.putStrLn "Bot starting!"
-  token <- TIO.readFile "./auth-token.secret"
-  userFacingError <-
-    runDiscord $
-      def
-        { discordToken = token,
-          discordOnStart = startHandler,
-          discordOnEvent = eventHandler
-        }
+  token           <- TIO.readFile "./auth-token.secret"
+  userFacingError <- runDiscord $ def { discordToken   = token
+                                      , discordOnStart = startHandler
+                                      , discordOnEvent = eventHandler
+                                      }
   TIO.putStrLn userFacingError
 
 startHandler :: DiscordHandler ()
 startHandler = do
-  let activity =
-        Activity
-          { activityName = "avansert rørlegging",
-            activityType = ActivityTypeGame,
-            activityUrl = Nothing
-          }
-  let opts =
-        UpdateStatusOpts
-          { updateStatusOptsSince = Nothing,
-            updateStatusOptsGame = Just activity,
-            updateStatusOptsNewStatus = UpdateStatusOnline,
-            updateStatusOptsAFK = False
-          }
+  let activity = Activity { activityName = "avansert rørlegging"
+                          , activityType = ActivityTypeGame
+                          , activityUrl  = Nothing
+                          }
+  let opts = UpdateStatusOpts { updateStatusOptsSince     = Nothing
+                              , updateStatusOptsGame      = Just activity
+                              , updateStatusOptsNewStatus = UpdateStatusOnline
+                              , updateStatusOptsAFK       = False
+                              }
   sendCommand (UpdateStatus opts)
 
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
-  MessageCreate m -> when (not (fromBot m) && isCommand (messageText m)) $ do
-    _ <- restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
+  MessageCreate m -> unless (fromBot m) $ do
+    when (mentionsMe (messageText m)) $ do
+      _ <- restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
+      pure ()
     -- threadDelay (4 * 10^6)
-    _ <-
-      restCall
-        (R.CreateMessage (messageChannel m) (parseAndExecute (messageText m)))
-    pure ()
-  _ -> pure ()
+    when (isCommand (messageText m)) $ do
+      _ <- execute m
+      pure ()
 
 fromBot :: Message -> Bool
 fromBot m = userIsBot (messageAuthor m)
--- src/Command.hs
+++ src/Command.hs
@@ -1,72 +1,111 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Command
-  ( isCommand,
-    getCommand,
-    parseAndExecute,
-  )
-where
+  ( isCommand
+  , getCommand
+  , mentionsMe
+  , execute
+  ) where
 
-import Data.Text (Text)
-import qualified Data.Text as T
-import Text.Parsec
-import Text.Printf
+import           Control.Monad.Trans            ( lift )
+import qualified Data.Map                      as M
+import           Data.Text                      ( Text )
+import qualified Data.Text                     as T
+import           Text.Printf
 
-data Command
-  = Echo Text
-  | Ping
-  deriving (Show, Eq)
+import           Discord
+import qualified Discord.Requests              as R
+import           Discord.Types
+
+import           System.Random
+
+-- Some helper functions
 
 isCommand :: Text -> Bool
 isCommand = ("!" `T.isPrefixOf`)
 
+mentionsMe :: Text -> Bool
+mentionsMe text' = count > 0
+ where
+  count = T.count "rørlegger" text + T.count "bot" text
+  text  = T.toLower text'
+
 getCommand :: Text -> Text
 getCommand = T.drop 1 . head . T.words
 
--- stolen from StackOverflow
-type Parser = Parsec T.Text ()
+getArgString :: Text -> Text
+getArgString = T.unwords . tail . T.words
 
-type GenParser t st = Parsec T.Text st
+-- The commands!
 
--- Wrapper funs
-parseAndExecute :: Text -> Text
-parseAndExecute cmd = either (T.pack . show) exec $ parseStuff cmd
+commandList :: [(Text, Message -> DiscordHandler ())]
+commandList =
+  [ ("echo" , echo)
+  , ("ekko" , echo)
+  , ("ping" , ping)
+  , ("roll" , roll)
+  , ("help" , help)
+  , ("hjelp", help)
+  , ("test" , test)
+  ]
 
-parseMaybe :: Text -> Maybe Command
-parseMaybe = rightToMaybe . parseStuff
+commands = M.fromList commandList
 
-rightToMaybe :: Either a b -> Maybe b
-rightToMaybe (Left _) = Nothing
-rightToMaybe (Right val) = Just val
+execute m = do
+  maybe (reportError "No such command" m) (\f -> f m) res
+  where res = M.lookup (getCommand $ messageText m) commands
 
-parseStuff :: Text -> Either ParseError Command
-parseStuff = parse parseCommand "(what)"
+helpText =
+  "Rørleggeren støtter følgende kommandoer:\n\
+  \`echo <whatever>` sier det du vil tilbake, alias `ekko`\n\
+  \`roll` for å kaste terning\n\
+  \`ping` ... pong"
 
-whitespace = skipMany $ oneOf " \t\n\r"
+reportError err m = do
+  _ <- restCall (R.CreateMessage (messageChannel m) err)
+  pure ()
 
-parseCommand :: Parser Command
-parseCommand = do
-  _ <- char '!'
-  choice [parsePing, parseEcho]
+help m = do
+  _ <- restCall (R.CreateMessage (messageChannel m) helpText)
+  pure ()
 
-parseArgs = many (whitespace *> many1 (noneOf " "))
+ping m = do
+  _ <- restCall (R.CreateMessage (messageChannel m) "pong")
+  pure ()
 
-parseInt :: Parser Int
-parseInt = do
-  num <- many1 (oneOf "1234567890")
-  return $ read num
+echo m = do
+  _ <- if length (T.words $ messageText m) > 1
+    then restCall
+      (R.CreateMessage (messageChannel m) (getArgString (messageText m)))
+    else restCall
+      (R.CreateMessage (messageChannel m) "Echo requires an argument")
+  pure ()
 
-parsePing :: Parser Command
-parsePing = do
-  _ <- string "ping"
-  return Ping
+roll m = do
+  num <- lift $ randomRIO (1, 6 :: Int)
+  _   <- restCall
+    (R.CreateMessage (messageChannel m)
+                     (T.concat [":game_die: **", T.pack $ show num, "**"])
+    )
+  pure ()
 
-parseEcho :: Parser Command
-parseEcho = do
-  _ <- string "echo"
-  args <- parseArgs -- todo these are not just args though
-  return $ Echo (T.unwords $ map T.pack args)
-
-exec :: Command -> Text
-exec (Echo text) = text
-exec Ping = "pong"
+test m = do
+  _ <- restCall
+    (R.CreateMessageEmbed (messageChannel m) "" $ def
+      { createEmbedTitle       = "Jensens rørleggerservice"
+      , createEmbedDescription = "Vi leverer bare rør"
+      , createEmbedThumbnail   =
+        Just
+          $ CreateEmbedImageUrl
+              "https://files.solvecms.com/test/7311e68/medium/jensen%20r%C3%B8rleggerservice%20logo.jpg?v=1519740759023"
+      , createEmbedImage       =
+        Just
+          $ CreateEmbedImageUrl
+              "https://files.solvecms.com/test/7311e68/medium/jensen%20r%C3%B8rleggerservice%20logo.jpg?v=1519740759023"
+      , createEmbedFields      = [ EmbedField "Dette" "er"   (Just True)
+                                 , EmbedField "en"    "test" (Just True)
+                                 , EmbedField "eller" "hva?" (Just True)
+                                 ]
+      }
+    )
+  pure ()
